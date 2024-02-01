234aaac0e69092b733886c148bdbed838107b49f
Angel Bran <angel.bran@goes.gob.sv>
2024-02-01 09:59:02 -0600

    Add: validando rutas a solo ids

--- src/routes/character/[id=integer]/+page.js
+++ src/routes/character/[id=integer]/+page.js
@@ -2,7 +2,7 @@ import { api } from '$lib/index';
 
 /** import('./$types.js').PageLoadData */
 export async function load({ fetch, params }) {
-    const res = await fetch(`${api}/character/${params.slug}`);
+    const res = await fetch(`${api}/character/${params.id}`);
     const data = await res.json();
 
     return { character: data };
